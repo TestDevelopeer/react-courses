ab1b9904f70c314dcc71c493c2416ea86a9e6d01
Igor <testdeveloper7967@gmail.com>
2021-12-04 17:24:58 +0300

    HOC: withLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Htag, P, Tag, Rating } from '../styles/components';
+import { Button, Htag, P, Tag, Rating } from '../components';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
 
 	const [counter, setCounter] = useState<number>(0);
 	const [rating, setRating] = useState<number>(4);
@@ -26,3 +27,5 @@ export default function Home(): JSX.Element {
 		</>
 	);
 }
+
+export default withLayout(Home);
